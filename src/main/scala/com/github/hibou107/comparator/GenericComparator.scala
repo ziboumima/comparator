c4355e60b489e23db13ff669c7426b71d2d2d5a3
Nam <hibou107@gmail.com>
2018-03-13 11:33:41 +0100

    finished

--- src/main/scala/com/github/hibou107/comparator/GenericComparator.scala
+++ src/main/scala/com/github/hibou107/comparator/GenericComparator.scala
@@ -1,11 +1,13 @@
 package com.github.hibou107.comparator
 
 import shapeless.labelled.FieldType
-import shapeless.{ :+:, ::, Coproduct, HList, HNil, Inl, Inr, LabelledGeneric, Lazy, Witness }
+import shapeless.{:+:, ::, CNil, Coproduct, HList, HNil, Inl, Inr, LabelledGeneric, Lazy, Witness}
 
 object GenericComparator {
 
   implicit val hNilComparator: Comparator[HNil] = (left: HNil, right: HNil) => Nil
+
+  implicit val cNilComparator: Comparator[CNil] = (left: CNil, right: CNil) => Nil
 
   implicit def hlistFieldComparator[K <: Symbol, H, T <: HList](implicit
     witness: Witness.Aux[K],
@@ -20,6 +22,20 @@ object GenericComparator {
     }
   }
 
+  implicit def coProductComparator[K <: Symbol, H, T <: Coproduct](
+                                                                    implicit
+                                                                    witness: Witness.Aux[K],
+                                                                    hComparator: Lazy[Comparator[H]],
+                                                                    tComparator: Comparator[T]
+                                                               ): Comparator[FieldType[K, H] :+: T] =
+      (left: FieldType[K, H] :+: T, right: FieldType[K, H] :+: T) => {
+        (left, right) match {
+          case (Inl(h1), Inl(h2)) => hComparator.value.compare(h1, h2)
+          case (Inr(h1), Inr(h2)) => tComparator.compare(h1, h2)
+          case (x, y)             => Diff(Nil, TypeDiff(x.toString, y.toString)) :: Nil
+        }
+      }
+
   implicit def coProductComparator[H, T <: Coproduct](implicit hComparator: Lazy[Comparator[H]], tComparator: Comparator[T]): Comparator[H :+: T] =
     (left: :+:[H, T], right: :+:[H, T]) => {
       (left, right) match {
@@ -31,10 +47,8 @@ object GenericComparator {
   implicit def genericComparator[A, H](implicit
     generic: LabelledGeneric.Aux[A, H],
     hEncoder: Lazy[Comparator[H]]): Comparator[A] =
-    new Comparator[A] {
-      override def compare(left: A, right: A): List[Diff] = {
-        hEncoder.value.compare(generic.to(left), generic.to(right))
-      }
+    (left: A, right: A) => {
+      hEncoder.value.compare(generic.to(left), generic.to(right))
     }
 
 }
